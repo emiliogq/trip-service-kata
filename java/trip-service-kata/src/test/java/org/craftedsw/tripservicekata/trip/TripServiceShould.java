ee5b5048d7a0ade874bb98a59ba438c05225b290
Emilio Garcia <github@emiliogq.com>
2022-06-08 07:32:38 +0200

    Add no trips with a user without friends
    The user logged in the system is a registered user
    Make use of internal attributes (TripServiceShould) to change the output of TestableTripService at each test case

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -4,22 +4,36 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TripServiceShould {
 
+    private static final User REGISTERED = new User();
+    private User loggedUser;
+
     @Test
 	void throwUserNotLoggedInWithANonLoggedUser(){
         TripService tripService = new TestableTripService();
-
+        this.loggedUser = null;
         assertThrows(UserNotLoggedInException.class, () -> tripService.getTripsByUser(null));
+    }
+
+    @Test
+    void getNoTripsWithAUserWithoutFriends(){
+        TripService tripService = new TestableTripService();
+        User anonymous = new User();
+        this.loggedUser = REGISTERED;
+        List<Trip> trips = tripService.getTripsByUser(anonymous);
+        assertTrue(trips.isEmpty());
     }
 
     protected class TestableTripService extends TripService {
         @Override
         protected User getLoggedUser(){
-            return null;
+            return loggedUser;
         }
     }
 }
